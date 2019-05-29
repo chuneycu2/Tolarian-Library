a8c900b4ba2a25a3affd75a1fb5d902ce02971a7
cyrus <48845648+chuneycu2@users.noreply.github.com>
2019-05-29 09:04:48 -0400

    test phase: search bar styles set up

--- resources/js/tolarian.js
+++ resources/js/tolarian.js
@@ -4,7 +4,7 @@ const API_KEY= '';
 
 TolarianLibrary.testAjax = function() {
   $.ajax({
-    url: '' + API_KEY + '',
+    url: '',
     success: function(response) {
       console.log(response);
     }
