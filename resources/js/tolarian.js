dd6af142240c908a53d1d0b326642477c4670eb0
cyrus <48845648+chuneycu2@users.noreply.github.com>
2019-06-04 14:19:18 -0400

    advanced search function flow complete, support functions in progress

--- resources/js/tolarian.js
+++ resources/js/tolarian.js
@@ -1,48 +1,123 @@
 var TolarianLibrary = {};
 
+var magicAPI = 'https://api.magicthegathering.io/v1/cards';
+
 $(document).ready(function() {
+
+  //sets a click handler for the main search icon
   $('#search-button').on('click', function() {
-    TolarianLibrary.ajaxRequest($('#search-input').val());
-  });
-  $('#advanced-search-button').on('click', function() {
-    TolarianLibrary.ajaxRequest($('#search-input').val());
+    TolarianLibrary.ajaxRequest(magicAPI + '?name' + $('#search-input').val());
   });
 
+  //enables the Enter key to perform a generic search
+  $(document).keypress(function(e) {
+    var key = e.which;
+    if (key === 13) {
+      TolarianLibrary.ajaxRequest(magicAPI + '?name' + $('#search-input').val());
+    };
+  });
+
+  //sets a click handler for the advanced search button
+  $('#advanced-search-button').on('click', function() {
+    TolarianLibrary.advancedSearch();
+  });
+
+  //sets click handlers for the options tabs
   $('#advanced-search').on('click', function() {
-    $('#advanced-search').toggleClass('rest').toggleClass('active');
+    $('#generate-booster')
+      .removeClass('active')
+      .addClass('rest');
+    $('.generate-booster').css({ "display": "none" });
+    $('#advanced-search')
+      .toggleClass('rest')
+      .toggleClass('active');
     $('.advanced-search').slideToggle('hide');
+  });
+
+  $('#generate-booster').on('click', function() {
+    $('#advanced-search')
+      .removeClass('active')
+      .addClass('rest');
+    $('.advanced-search').css({ "display": "none" });
+    $('#generate-booster')
+      .toggleClass('rest')
+      .toggleClass('active');
+    $('.generate-booster').slideToggle('hide');
+  });
+
+  var $checkbox = $('input[type=checkbox]');
+
+  var selectedColors = '';
+
+  $checkbox.on('click', function() {
+    if ($(this).prop('checked', false)) {
+      $(this).prop('checked', true);
+      console.log("it's checked!");
+      selectedColors += this.id;
+    }
+    console.log(selectedColors);
   });
 
 });
 
-/* $(document).ready(function() {
+TolarianLibrary.selectedColors = function() {
 
-  /* $('#search-button').on('click', function() {
-    TolarianLibrary.ajaxRequest($('#search-input').val());
-  });
+  $checkbox = $('input[type=checkbox]');
 
-  $(document).keypress(function(e) {
-    var key = e.which;
-    if (key === 13) {
-      TolarianLibrary.ajaxRequest($('#search-input').val());
-    };
-  });
+  const selectedColors = '';
 
-  var $body = $('body');
-  var $cardList = $('#card-list');
-
-  $(document).on({
-    ajaxStart: function() {
-      $body.removeClass('relative');
-      $cardList.empty();
-      $body.addClass('loading');
-    },
-    ajaxStop: function() {
-      $body.removeClass('loading');
+  $checkbox.on('click', function() {
+    if ($('input[type=checkbox]:checked')) {
+      console.log("it's checked!")
     }
-  })
+    console.log(selectedColors);
+  });
+  //return a string of selected color words
+}
 
-}); */
+TolarianLibrary.advancedSearch = function() {
+
+  var magicAPIparams = 'https://api.magicthegathering.io/v1/cards?';
+
+  var $name = $('#card-name').val();
+  var $cmc = $('#cmc').val();
+  var $text = $('#text').val();
+  var $types = $('#types').val();
+  //var $colors = TolarianLibrary.selectedColors(); //returns a string of color words
+  var $rarity = $('#rarity').val();
+  var $set = $('#set').val();
+  var $power = $('#power').val();
+  var $toughness = $('#toughness').val();
+  var $artist = $('#artist').val();
+  var $language = $('#language').val();
+
+  const searchEntries = {
+    name: $name,
+    cmc: $cmc,
+    text: $text,
+    types: $types,
+    //colors: $colors,
+    rarity: $rarity,
+    set: $set,
+    power: $power,
+    toughness: $toughness,
+    artist: $artist,
+    language: $language
+  };
+
+  for (key in searchEntries) {
+    if (searchEntries[key] == '') {
+      delete searchEntries[key];
+    }
+  }
+
+  console.log(searchEntries);
+
+  console.log($.param(searchEntries));
+
+  var searchUrl = '';
+}
+
 
 //when called, the card array and the current index of that array are sent as parameters
 TolarianLibrary.renderRulings = function(cards, index) {
@@ -155,9 +230,9 @@ TolarianLibrary.renderCards = function(cards) {
 
 };
 
-TolarianLibrary.ajaxRequest = function(name) {
+TolarianLibrary.ajaxRequest = function(url) {
   $.ajax({
-    url: 'https://api.magicthegathering.io/v1/cards?name=' + name,
+    url: url,
     type: 'GET',
     dataType: 'JSON',
     success: function(response) {
